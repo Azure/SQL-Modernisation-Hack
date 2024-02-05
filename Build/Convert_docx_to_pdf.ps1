cda0da0dc06b72427c7c9dc0efb492f65fd115f3
mesengne <58394518+mesengne@users.noreply.github.com>
2024-02-05 11:52:57 +0100

    Other_Files

---
+++ Build/Convert_docx_to_pdf.ps1
@@ -0,0 +1,18 @@
+﻿
+$folder= 'LABS'
+$documents_path= (Join-Path $CurrentDir $folder)
+
+$word_app = New-Object -ComObject Word.Application
+
+Get-ChildItem -Path $documents_path -Filter *.docx -Recurse| ForEach-Object {
+
+    $document = $word_app.Documents.Open($_.FullName)
+
+    $pdf_filename = "$($_.DirectoryName)\$($_.BaseName).pdf"
+
+    $document.SaveAs([ref] $pdf_filename, [ref] 17)
+    Write-host "$pdf_filename converted from docx"
+    $document.Close()
+}
+
+$word_app.Quit()
