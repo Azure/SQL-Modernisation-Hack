08c5e7301d2e13a44cae40868144deb1fe496220
mesengne <58394518+mesengne@users.noreply.github.com>
2024-02-05 11:50:30 +0100

    OtherFiles

---
+++ CreateAdUsersAndPermissions.ps1
@@ -0,0 +1,66 @@
+﻿#Connect-AzAccount
+#Install-Module -Name AzureAD
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+
+$user_group="SQLHackUsers"
+$SharedRG ="SQLHACK-SHARED"
+$VmsRG ="SQLHACK-TEAM_VMs"
+
+$group = get-AzAdgroup -DisplayName $user_group
+
+if(!$group)
+{
+
+$group= New-AzADGroup -DisplayName $user_group -MailNickname $user_group
+}
+$username="sqlhackuser"
+$domain="1dayopenhack.biz"
+
+$count=1..25
+
+$groupid=$group.Id
+
+foreach ($i in $count)
+{
+    $i_string = $i | % tostring 00
+    $password="&sqlhack@demo_"+$i_string+"!"
+    $securepassword= ConvertTo-SecureString $password -AsPlainText -force
+    $upn=$username+$i_string +"@"+$domain
+    $member=New-AzADUser -DisplayName $username$i_string  -Password $securepassword  -AccountEnabled $true -MailNickname $username$i_string  -UserPrincipalName $upn
+    Add-AzADGroupMember -TargetGroupObjectId $groupid -MemberObjectId $member.Id
+     
+    }
+# Create SQL MI Admin
+    $username ="sqlmiadmin"
+    $password = "Foso2272#OH2903"
+    $securepassword= ConvertTo-SecureString $password -AsPlainText -force
+    $upn=$username+"@"+$domain
+    $member=New-AzADUser -DisplayName $username$i_string  -Password $securepassword  -AccountEnabled $true -MailNickname $username$i_string  -UserPrincipalName $upn
+
+
+    $username ="adminuser"
+    $password = "#SQLm2cAdmin@oh!!"
+    $securepassword= ConvertTo-SecureString $password -AsPlainText -force
+    $upn=$username+"@"+$domain
+    $member=New-AzADUser -DisplayName $username$i_string  -Password $securepassword  -AccountEnabled $true -MailNickname $username$i_string  -UserPrincipalName $upn
+
+#Get-AzRoleDefinition | Format-Table -Property Name, IsCustom, Id
+
+New-AzRoleAssignment -ObjectId $groupid -RoleDefinitionName Reader -Scope (Get-AzResourceGroup -Name $SharedRG).ResourceId
+New-AzRoleAssignment -ObjectId $groupid -RoleDefinitionName Reader -Scope (Get-AzResourceGroup -Name $VmsRG).ResourceId
+
+
+#get SQL MI Name
+$sqlmi = Get-AzSqlInstance -ResourceGroupName $SharedRG | Select-object -First 1
+New-AzRoleAssignment -ObjectId $groupid -RoleDefinitionName Contributor -Scope ($sqlmi).Id
+#Get storage account info
+$StorageAccount = Get-AzStorageAccount -ResourceGroupName $SharedRG | Select-object -First 1
+New-AzRoleAssignment -ObjectId $groupid -RoleDefinitionName Contributor -Scope ($StorageAccount).Id
+
+
+# assign contributor on DMS (classic)
+New-AzRoleAssignment -ObjectId $groupid -RoleDefinitionName Contributor -Scope (Get-AzResource -Name sqlhack-migrationservice).ResourceId
+
+# assign contributor on DMS (new)
+New-AzRoleAssignment -ObjectId $groupid -RoleDefinitionName Contributor -Scope (Get-AzResource -Name sqlhack-dmsV2).ResourceId
---
+++ Launch_SQL_MI_configuration.ps1
@@ -0,0 +1,68 @@
+﻿<# A
+#POWERSHELL ENVIRONMENTALS:
+#=========================
+
+#1. SET PS SCRIPT SECURITY:
+#1.1 Set PS execution policy to Unrestricted so script can be run:
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+#1.1 Confirm Exectution Policy changed to Unrestricted:
+Get-ExecutionPolicy
+
+#2. CONNECT PS SESSION TO TARGET AZURE SUBSCRIPTION:
+#2.1 Run this to connect into Azure:
+Connect-AzAccount
+
+#2.2 Run this to get a list of Subscriptions you have access to:
+Get-AzSubscription
+
+#2.3 Replace <Tenant ID> and <Subscription ID> placeholders below then run: 
+Select-AzSubscription -Tenant '<Tenant ID>' -SubscriptionId '<Subscription ID>'
+
+#NOW RUN THE ENTIRE PS SCRIPT COMPLETING REQUESTED PARAMTERS AS PROMPTED.
+#>
+#Select-AzSubscription -Tenant '72f988bf-86f1-41af-91ab-2d7cd011db47' -SubscriptionId 'ab6dbbb5-ff85-4692-a99c-490f66eed14a'
+#Select-AzSubscription -Tenant '3149cb37-c8a4-473d-becf-5b22cce27182' -SubscriptionId '4cbf3664-3873-4cd2-bd36-129282d24d71'
+
+$CurrentDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+$TeamRG = "SQLHACK-TEAM_VMs"
+$SharedRG ="SQLHACK-SHARED"
+$VWMName = "vm-TEAM19" #VM used to execute the configuration
+$adminUsername="DemoUser"
+$adminPassword = Read-Host "Please enter a 16 character Password. The password must be between 16 and 128 characters in length and must contain at least one number, one non-alphanumeric character, and one upper or lower case letter" 
+
+$Script = 'Powershell/Configure-SQL_MI.ps1'
+$ScriptPath= (Join-Path $CurrentDir $Script)
+
+
+#get SQL MI FQDN
+$sqlmiFDQN = (Get-AzSqlInstance -ResourceGroupName $SharedRG).FullyQualifiedDomainName  | Select-object -First 1
+
+#Get storage account info
+$StorageAccount = (Get-AzStorageAccount -ResourceGroupName $SharedRG).StorageAccountName  | Select-object -First 1
+$StorageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $SharedRG -Name $StorageAccount
+$Key0 = $StorageAccountKeys | Select-Object -First 1 -ExpandProperty Value
+$Context = New-AzStorageContext -StorageAccountName $StorageAccount -StorageAccountKey $Key0
+$SASURIKey= (New-AzStorageContainerSASToken -Name "build" -Policy "Build-Policy" -Context $Context).TrimStart("?")
+
+$JsonSASUriContainerBuild = $SASURIKey | ConvertTo-Json
+
+$Message = "$(get-date -Format 'dd/MM/yyyy hh:mm:ss'): $VMName -- Configuration starting..."
+write-host $Message
+$out = Invoke-AzVMRunCommand -ResourceGroupName $TeamRG -Name $VWMName -CommandId RunPowerShellScript -ScriptPath $ScriptPath -Parameter @{SharedRG =$SharedRG ;adminUsername = $adminUsername;adminPassword = $adminPassword; StorageAccount = $StorageAccount; SASURIKey = $JsonSASUriContainerBuild; sqlmiFDQN  = $sqlmiFDQN }
+
+if($out.value[1].Message)
+{
+    $status= "failed" 
+    $ForegroundColor="Red"
+    $message = $out.value[1].Message
+}
+else {
+    $status= "successfull"
+    $ForegroundColor="White"
+    $message = $out.value[0].Message
+}
+$output =  "$(get-date -Format 'dd/MM/yyyy hh:mm:ss'): $VMName -- status: $status " + $message
+Write-host $output - -ForegroundColor  $ForegroundColor
+
